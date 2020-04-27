a08f23509153e1029850bb263fafac67a6e0988b
RunLittleRabbit <zonarismaxim@gmail.com>
2020-04-27 17:17:22 +0300

    push build

---
+++ client/build/precache-manifest.79afc1f945022c56e02f4e2b5dfe33b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92f9c7b0a30a14c5e15c504a54cb923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8610d78430142cf307d",
+    "url": "/static/css/main.b682bef8.chunk.css"
+  },
+  {
+    "revision": "afdfa274fe264c9682b1",
+    "url": "/static/js/2.71390ca2.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.71390ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8610d78430142cf307d",
+    "url": "/static/js/main.06d0e4eb.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
